d652386f92f28c3ba19796947174dd72d169d6eb
CN.QS <cnsongzi@foxmail.com>
2014-04-10 17:16:40 +0800

    修改article_edit为编辑文章页面

--- application/views/Manage/Article/article_edit.php
+++ application/views/Manage/Article/article_edit.php
@@ -13,14 +13,14 @@
         <ul id="dashboard-menu">
             
             <li >
-                <a class="dropdown-toggle" href="#">
+                <a>
                     <i class="icon-edit"></i>
                     <span>文章管理</span>
                     <i class="icon-chevron-down"></i>
                 </a>
-                <ul class="submenu">
-                    <li  class="active"><a href="form-showcase.html">写文章</a></li>
-                    <li><a href="form-wizard.html">目录分类</a></li>
+                <ul>
+                    <li><a href="articleAdd">写文章</a></li>
+                    <li><a href="categoryList">目录分类</a></li>
                 </ul>
             </li>         
           
@@ -32,165 +32,57 @@
 	<!-- main container -->
     <div class="content">
         
-        <!-- settings changer -->
-        <div class="skins-nav">
-            <a href="#" class="skin first_nav selected">
-                <span class="icon"></span><span class="text">Default</span>
-            </a>
-            <a href="#" class="skin second_nav" data-file="css/skins/dark.css">
-                <span class="icon"></span><span class="text">Dark skin</span>
-            </a>
-        </div>
-        
+  
         <div class="container-fluid">
             <div id="pad-wrapper">
                 
                 <!-- products table-->
                 <!-- the script for the toggle all checkboxes from header is located in js/theme.js -->
-                <div class="table-wrapper products-table section">
-                    <div class="row-fluid head">
-                        <div class="span12">
-                            <h4>文章列表</h4>
-                        </div>
-                    </div>
-
-                    <div class="row-fluid filter-block">
-                        <div class="pull-right">
-                            <div class="ui-select">
-                                <select>
-                                  <option />Filter users
-                                  <option />Signed last 30 days
-                                  <option />Active users
-                                </select>
-                            </div>
-                            <input type="text" class="search" />
-                            <a class="btn-flat success new-product">+ Add product</a>
-                        </div>
-                    </div>
-
-                    <div class="row-fluid">
-                        <table class="table table-hover">
-                            <thead>
-                                <tr>
-                                    <th class="span3">
-                                        <input type="checkbox" />
-                                        Product
-                                    </th>
-                                    <th class="span3">
-                                        <span class="line"></span>Description
-                                    </th>
-                                    <th class="span3">
-                                        <span class="line"></span>Status
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <!-- row -->
-                                <tr class="first">
-                                    <td>
-                                        <input type="checkbox" />
-                                        <div class="img">
-                                            <img src="img/table-img.png" />
-                                        </div>
-                                        <a href="#" class="name">Generate Lorem </a>
-                                    </td>
-                                    <td class="description">
-                                        if you are going to use a passage of Lorem Ipsum.
-                                    </td>
-                                    <td>
-                                        <span class="label label-success">Active</span>
-                                        <ul class="actions">
-                                            <li><a href="#">Edit</a></li>
-                                            <li class="last"><a href="#">Delete</a></li>
-                                        </ul>
-                                    </td>
-                                </tr>
-                                <!-- row -->
-                                <tr>
-                                    <td>
-                                        <input type="checkbox" />
-                                        <div class="img">
-                                            <img src="img/table-img.png" />
-                                        </div>
-                                        <a href="#" class="name">Internet tend</a>
-                                    </td>
-                                    <td class="description">
-                                        There are many variations of passages.
-                                    </td>
-                                    <td>
-                                        <ul class="actions">
-                                            <li><a href="#">Edit</a></li>
-                                            <li class="last"><a href="#">Delete</a></li>
-                                        </ul>
-                                    </td>
-                                </tr>
-                                <!-- row -->
-                                <tr>
-                                    <td>
-                                        <input type="checkbox" />
-                                        <div class="img">
-                                            <img src="img/table-img.png" />
-                                        </div>
-                                        <a href="#" class="name">Generate Lorem </a>
-                                    </td>
-                                    <td class="description">
-                                        if you are going to use a passage of Lorem Ipsum.
-                                    </td>
-                                    <td>
-                                        <ul class="actions">
-                                            <li><a href="#">Edit</a></li>
-                                            <li class="last"><a href="#">Delete</a></li>
-                                        </ul>
-                                    </td>
-                                </tr>
-                                <!-- row -->
-                                <tr>
-                                    <td>
-                                        <input type="checkbox" />
-                                        <div class="img">
-                                            <img src="img/table-img.png" />
-                                        </div>
-                                        <a href="#" class="name">Internet tend</a>
-                                    </td>
-                                    <td class="description">
-                                        There are many variations of passages.
-                                    </td>
-                                    <td>
-                                        <span class="label label-info">Standby</span>
-                                        <ul class="actions">
-                                            <li><a href="#">Edit</a></li>
-                                            <li class="last"><a href="#">Delete</a></li>
-                                        </ul>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <input type="checkbox" />
-                                        <div class="img">
-                                            <img src="img/table-img.png" />
-                                        </div>
-                                        <a href="#" class="name">Generate Lorem </a>
-                                    </td>
-                                    <td class="description">
-                                        if you are going to use a passage of Lorem Ipsum.
-                                    </td>
-                                    <td>
-                                        <span class="label label-success">Active</span>
-                                        <ul class="actions">
-                                            <li><a href="#">Edit</a></li>
-                                            <li class="last"><a href="#">Delete</a></li>
-                                        </ul>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
+                <div id="title">标题：<input class="input-xxlarge" type="text" ></div>
+				
+                <div id="author">作者：：<input  type="text" ></div>
+                
+				<div id="category">分类：
+                <select>
+					<option>1</option>
+                    <option>2</option>
+				</select>
                 </div>
-                <!-- end products table -->
+			
+			  <div id="content" style="margin:10px 0 auto">
+				<p>文章内容：
+			    <textarea class="summernote"></textarea>                
+			  </div>
+              
+              <div id="button">
+              	<button class="btn btn-primary">发布</button>
+                <button class="btn btn-primary">重置</button>
+              </div>
 
               
             </div>
         </div>
     </div>
 	
-    <script src="<?=base_url()?>static/admin/js/theme.js"></script>
+	<link rel="stylesheet" href="<?=base_url()?>static/summernote/summernote.css">
+	<script type="text/javascript" src="<?=base_url()?>static/summernote/summernote.js"></script>
+	
+	<script>
+		 $(function() {
+      $('.summernote').summernote({
+        height: 200,
+		width:800,
+		
+		toolbar: [
+   
+    ['style', ['bold', 'italic', 'underline', 'clear']],
+    ['font', ['strike']],
+    ['fontsize', ['fontsize']],
+    ['color', ['color']],
+    ['para', ['ul', 'ol', 'paragraph']],
+    ['height', ['height']],
+  
+  ]
+      });
+    });
+	</script>
