eabe99ea31a090a0e397a8aa27ea1f26ce400d8f
CN.QS <cnsongzi@foxmail.com>
2014-04-16 17:05:18 +0800

    拆分页面成nav和body

---
+++ application/views/Activity/body.php
@@ -0,0 +1,50 @@
+<div class="row">
+		<div class="span12">
+			<div class="hero-unit">
+			<h1>Heading</h1>
+			<p>Tagline</p>
+			<p>
+			<a class="btn btn-primary btn-large">
+				Learn more
+			</a>
+			</p>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="span4">
+			<ul class="thumbnails">
+			<li class="span4">
+				<a href="#" class="thumbnail">
+				<img src="<?=base_url('static/img/320.png')?>" alt="">
+				</a>
+			</li>
+			...
+			</ul>
+		</div>
+		<div class="span4">
+			<ul class="thumbnails">
+			<li class="span4">
+				<a href="#" class="thumbnail">
+				<img src="<?=base_url('static/img/320.png')?>" alt="">
+				</a>
+			</li>
+			...
+			</ul>
+		</div>
+		<div class="span4">
+			<ul class="thumbnails">
+			<li class="span4">
+				<a href="#" class="thumbnail">
+				<img src="<?=base_url('static/img/320.png')?>" alt="">
+				</a>
+			</li>		
+			</ul>
+		</div>
+	</div>
+	<div class="row">
+		<div class="span12">
+		</div>
+	</div>
+</div>
+</body>
